67fbfaeaf3a8cba3152d37b9c1c442c994b10934
thokozanisibeko111-debug <thokozani.sibeko111@gmail.com>
2026-01-30 12:09:27 -0800

    Fix admin access effect dependencies

--- app/admin/topics/[id]/page.tsx
+++ app/admin/topics/[id]/page.tsx
@@ -106,7 +106,7 @@ export default function AdminTopicPage() {
     }
 
     checkAccess();
-  }, [topicId]);
+  }, [supabase, supabaseConfigError, topicId]);
 
   useEffect(() => {
     if (isAuthorized && topicId) {
